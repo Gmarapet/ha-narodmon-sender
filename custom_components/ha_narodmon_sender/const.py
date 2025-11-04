a294c7275c7be1efb4408707dd633cbef9d54fe0
Gmarapet <gmarapet@gmail.com>
2025-11-05 01:56:33 +0300

    Back to tag v0.0.1

--- custom_components/ha_narodmon_sender/const.py
+++ custom_components/ha_narodmon_sender/const.py
@@ -1,34 +1,10 @@
 DOMAIN = "ha_narodmon_sender"
 
-CONF_INTERVAL = "interval"
-CONF_MAC = "mac_address"
 CONF_LATITUDE = "latitude"
 CONF_LONGITUDE = "longitude"
-CONF_ELEVATION = "elevation"
-CONF_GROUPS = "groups"
-CONF_CUSTOM = "custom_sensors"
+CONF_ALTITUDE = "altitude"
+CONF_MAC_ADDRESS = "mac_address"
 
-DEFAULT_INTERVAL = 300  # seconds (upload interval)
-SAMPLE_INTERVAL = 10    # seconds (how often to sample & compute group aggregate)
-MAX_BUFFER_MULTIPLIER = 3  # keep buffers reasonably bounded
+DEFAULT_UPDATE_INTERVAL = 360  # seconds (upload interval)
+DEFAULT_SEND_URL = "https://narodmon.ru/post"
 
-API_BASE = "https://narodmon.ru/post"
-
-SUPPORTED_GROUPS = {
-    "temperature": "TEMP",
-    "humidity": "RH",
-    "pressure": "PRESS",
-    "wind_speed": "WS",
-    "wind_dir": "DIR",
-    "lux": "LUX",
-    "radiation": "RAD",
-    "co": "CO",
-    "co2": "CO2",
-    "ch4": "CH4",
-    "ppm": "PPM",
-    "uv": "UV",
-    "pm": "PM",
-    "dew_point": "DP"
-}
-
-AGGREGATION_MODES = ["average", "minimum", "maximum"]
